81a61698b22ff1194e1324b9496dff9e26dd0d24
Xavier B <facedepete@live.ca>
2021-12-05 00:59:24 -0500

    Day 5

---
+++ day5/part2.py
@@ -0,0 +1,41 @@
+class Segment:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    def points(self):
+        if self.end[0] - self.start[0] != 0 and self.end[1] - self.start[1] != 0:
+            s1 = int(self.end[0] > self.start[0]) or -1
+            s2 = int(self.end[1] > self.start[1]) or -1
+            points = []
+            for i in range(abs(self.end[0] - self.start[0]) + 1):
+                x = s1 * i + self.start[0]
+                y = s2 * i + self.start[1]
+                points.append((x, y))
+            return points
+        elif self.end[0] - self.start[0] == 0:
+            return [(self.start[0], y) for y in
+                    range(min(self.start[1], self.end[1]), max(self.start[1], self.end[1]) + 1)]
+        else:
+            return [(x, self.start[1]) for x in
+                    range(min(self.start[0], self.end[0]), max(self.start[0], self.end[0]) + 1)]
+
+
+text = open('input.txt').readlines()
+segments = []
+
+for line in text:
+    line = line.strip().split(' -> ')
+    segments.append(Segment(eval(line[0]), eval(line[1])))
+
+board = {}
+for seg in segments:
+    intersection = seg.points()
+    for point in intersection:
+        board[point] = board.setdefault(point, 0) + 1
+
+i = 0
+for v in board.values():
+    if v > 1:
+        i += 1
+print(f'There are {i} points with at least 2 overlaps')
